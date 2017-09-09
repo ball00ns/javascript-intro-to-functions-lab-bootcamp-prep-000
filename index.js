85099a94d0360ed884ac44cf84bf4a1f14d83f8d
Jyn Ho <jynnn@live.com>
2017-09-09 15:45:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-function sayHiToGrandma(string.toLowerCase()){
-  return string
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase === string){
+    return "I can't hear you!"
+  }
+  elseif (string.toUpperCase === string){
+    return "YES INDEED!"
+  }
 }
